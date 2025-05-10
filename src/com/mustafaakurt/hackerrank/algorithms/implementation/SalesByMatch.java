13f0d7fdb3008530a89f68bf05539eb3c6ebde3a
mustafaakurt <mustafaakurt@yandex.com>
2025-05-10 15:40:40 +0300

    sales-by-match added

---
+++ src/com/mustafaakurt/hackerrank/algorithms/implementation/SalesByMatch.java
@@ -0,0 +1,58 @@
+package com.mustafaakurt.hackerrank.algorithms.implementation;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'sockMerchant' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY ar
+     */
+
+    public static int sockMerchant(int n, List<Integer> ar) {
+        Map<Integer, Integer> sockCountMap = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            sockCountMap.put(ar.get(i), sockCountMap.getOrDefault(ar.get(i), 0) + 1);
+        }
+
+        int pairCount = 0;
+        for (int color : sockCountMap.values()) {
+            pairCount += color / 2;
+        }
+        return pairCount;
+    }
+
+}
+
+public class SalesByMatch {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = Result.sockMerchant(n, ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
